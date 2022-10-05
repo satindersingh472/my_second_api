ac848acd61d7cf553222bc82f3de422cfd13a3c9
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-10-05 13:17:52 -0600

    updated but still working on

--- dbhelpers.py
+++ dbhelpers.py
@@ -1,23 +1,53 @@
+
 import mariadb
 import dbcreds
 
 def connect_db():
-    conn = mariadb.connect(user=dbcreds.user,host=dbcreds.host, password=dbcreds.password,port=dbcreds.port,database=dbcreds.database)
-    cursor = conn.cursor()
-    return cursor
-
+    try:
+        conn = mariadb.connect(user=dbcreds.user,host=dbcreds.host, password=dbcreds.password,port=dbcreds.port,database=dbcreds.database)
+        cursor = conn.cursor()
+        return cursor
+    except mariadb.ProgrammingError as error:
+        print('programming error: ',error)
+        return str(error)
+    except Exception as error:
+        print('Unknown Error', error)
+        return str(error)
 def execute_statement(cursor,statement,list=[]):
-    cursor.execute(statement,list)
-    result = cursor.fetchall()
-    return result
+    try:
+        cursor.execute(statement,list)
+        result = cursor.fetchall()
+        return result
+    except mariadb.InternalError as error:
+        print('Internal Error', error)
+        return str(error)
+    except mariadb.DatabaseError as error:
+        print('Database Error',error)
+        return str(error)
+    except mariadb.OperationalError as error:
+        print('Operational Error',error)
+        return str(error)
+    except mariadb.IntegrityError as error:
+        print('Integrity error',error)
+        return str(error)
 
 def close_connection(cursor):
-    conn = cursor.connection
-    cursor.close()
-    conn.close()
+    try:
+        conn = cursor.connection
+        cursor.close()
+        conn.close()
+    except mariadb.OperationalError as error:
+        print('Operational Error',error)
+        return str(error)
+    except Exception as error:
+        print('Unknown Error',error)
+        return str(error)
+
 
 def conn_exe_close(statement,list):
     cursor = connect_db()
+    if(cursor == None):
+        return 'Connection Error'
     result = execute_statement(cursor,statement,list)
     close_connection(cursor)
     return result
