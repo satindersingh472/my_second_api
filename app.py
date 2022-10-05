13f2c7cf5f3d5b80babfef96e38a834b17ee5e00
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-10-05 13:08:50 -0600

    working on

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from unittest import result
 from dbhelpers import conn_exe_close
 from flask import Flask, request
 import json
@@ -44,7 +45,30 @@ def delete_item():
     return results_json
 
 @app.get('/api/employee')
-def 
+def specific_employee():
+    id = request.json.get('id')
+    results_json = get_display_results('call specific_employee(?)',[id])
+    return results_json
 
+@app.post('/api/employee')
+def add_employee():
+    name = request.json.get('name')
+    position = request.json.get('position')
+    hourly_wage = request.json.get('hourly_wage')
+    results_json = get_display_results('call add_employee(?,?)',[name,position,hourly_wage])
+    return results_json
+
+@app.patch('/api/employee')
+def update_employee_wage():
+    id = request.json.get('id')
+    hourly_wage = request.json.get('hourly_wage')
+    results_json = get_display_results('call update_employee_Wage(?,?)',[id,hourly_wage])
+    return results_json
+
+@app.delete('/api/employee')
+def delete_employee():
+    id = request.json.get('id')
+    results_json = get_display_results('call delete_employee(?)',[id])
+    return results_json
 
 app.run(debug=True)
