afbbf7c8f26dbf43f6fc3360969fe23f8163ec90
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-10-05 22:17:25 -0600

    everything works but error not handled properly yet

--- app.py
+++ app.py
@@ -1,28 +1,42 @@
-
+# import one function from dbhelpers to use
+# connecting to database
+# getting the result or runnung the stored procedure
+# closing the connection
 from dbhelpers import conn_exe_close
+# import flask library
 from flask import Flask, request
+# import json to send results as a string
 import json
 
 app = Flask(__name__)
 
+# to run the main statement from dbhelpers
+# it will need the stored procedure and list of arguments
 def get_display_results(statement,params_list):
+    # execute the function imported from dbhelpers and store the result
     results = conn_exe_close(statement,params_list)
+    # result will be stored in list form
+    # if result is not there then it will show an error msg 
     if(type(results) == list):
-        results_json = json.dumps(results, default=str)
-        return results_json
+        if(len(results) > 0):
+            results_json = json.dumps(results, default=str)
+            return results_json
+        elif(len(results) == 0):
+            return 'there are 0 results matched the data provided'
+        # if not a list then probably error will be in a form of string
     elif(type(results) == str):
         return results
+        # for further error handling will rely on else
     else:
         return 'Sorry, An error occured'
 
-
+# it will get all the available items without any params
 @app.get('/api/items')
 def all_items_info():
     results_json  = get_display_results('call all_items_info()',[])
     return results_json
 
-all_items_info()
-
+# it will post the new item to the items table in database
 @app.post('/api/items')
 def add_new_item():
     name = request.json.get('name')
@@ -30,6 +44,7 @@ def add_new_item():
     in_stock_quantity = request.json.get('in_stock_quantity')
     results_json = get_display_results('call add_new_item(?,?,?)',[name,description,in_stock_quantity])
     return results_json
+
 
 @app.patch('/api/items')
 def update_quantity_item():
@@ -55,7 +70,7 @@ def add_employee():
     name = request.json.get('name')
     position = request.json.get('position')
     hourly_wage = request.json.get('hourly_wage')
-    results_json = get_display_results('call add_employee(?,?)',[name,position,hourly_wage])
+    results_json = get_display_results('call add_employee(?,?,?)',[name,position,hourly_wage])
     return results_json
 
 @app.patch('/api/employee')
@@ -68,7 +83,7 @@ def update_employee_wage():
 @app.delete('/api/employee')
 def delete_employee():
     id = request.json.get('id')
-    results_json = get_display_results('call delete_employee(?)',[id])
+    results_json =  get_display_results('call delete_employee(?)',[id])
     return results_json
 
 
--- dbhelpers.py
+++ dbhelpers.py
@@ -1,7 +1,9 @@
 
+# import database and credentials file
 import mariadb
 import dbcreds
 
+# connect to database with the help of dbcreds.py
 def connect_db():
     try:
         conn = mariadb.connect(user=dbcreds.user,host=dbcreds.host, password=dbcreds.password,port=dbcreds.port,database=dbcreds.database)
@@ -13,6 +15,9 @@ def connect_db():
     except Exception as error:
         print('Unknown Error', error)
         return str(error)
+
+#execute the given statement with cursor and stored procedure with list arguments 
+# return the result
 def execute_statement(cursor,statement,list=[]):
     try:
         cursor.execute(statement,list)
@@ -30,7 +35,17 @@ def execute_statement(cursor,statement,list=[]):
     except mariadb.IntegrityError as error:
         print('Integrity error',error)
         return str(error)
+    except mariadb.ProgrammingError as error:
+        print('Programming Error',error)
+        return str(error)
+    except TypeError as error:
+        print('Type Error',error)
+        return str(error)
+    except Exception as error:
+        print('Unknown Error',error)
+        return str(error)
 
+# close the connection after the data is fetched or running the execute statement
 def close_connection(cursor):
     try:
         conn = cursor.connection
@@ -43,11 +58,15 @@ def close_connection(cursor):
         print('Unknown Error',error)
         return str(error)
 
-
+# using all three functions in one function to shrink the commands needed each time to
+# ineract with database
 def conn_exe_close(statement,list):
     cursor = connect_db()
     if(cursor == None):
         return 'Connection Error'
     result = execute_statement(cursor,statement,list)
+    if(result == None):
+        close_connection()
+        return 'Error in performing a request'
     close_connection(cursor)
     return result
